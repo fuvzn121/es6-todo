b817404ee802daf56861c47591dee2442d937590
fuvzn121 <hrysm121@gmail.com>
2019-02-07 13:01:17 +0900

    done

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { render } from "./view/html-util.js";
+import { TodoListView } from "./view/TodoListView.js";
+import { TodoItemModel } from "./model/TodoItemModel.js";
+import { TodoListModel } from "./model/TodoListModel.js";
+
+export class App {
+  constructor() {
+    this.todoListView = new TodoListView();
+    this.todoListModel = new TodoListModel([]);
+  }
+
+  handleAdd(title) {
+    this.todoListModel.addTodo(new TodoItemModel({ title, completed: false }));
+  }
+
+  handleUpdate({ id, completed }) {
+    this.todoListModel.updateTodo({ id, completed });
+  }
+
+  handleDelete({ id }) {
+    this.todoListModel.deleteTodo({ id });
+  }
+
+  mount() {
+    const formElement = document.querySelector("#js-form");
+    const inputElement = document.querySelector("#js-form-input");
+    const todoCountElement = document.querySelector("#js-todo-count");
+    const todoListContainerElement = document.querySelector("#js-todo-list");
+    this.todoListModel.onChange(() => {
+      const todoItems = this.todoListModel.getTodoItems();
+      const todoListElement = this.todoListView.createElement(todoItems, {
+        onUpdateTodo: ({ id, completed }) => {
+          this.handleUpdate({ id, completed });
+        },
+        onDeleteTodo: ({ id }) => {
+          this.handleDelete({ id });
+        }
+      });
+      render(todoListElement, todoListContainerElement);
+      todoCountElement.textContent = `Todoアイテム数: ${this.todoListModel.totalCount}`;
+    });
+
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+      this.handleAdd(inputElement.value);
+      inputElement.value = "";
+    });
+  }
+}
